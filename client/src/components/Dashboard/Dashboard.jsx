904839f065884eedd3341f89808e57881a1035ed
kaussy6902 <mu.kaustav@gmail.com>
2021-08-19 01:33:15 +0530

    add dashboard frontend

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
+import loadingImg from '../../assets/profile_logo.png'
+import FeatureCard from '../FeatureCard/FeatureCard'
 import './dashboard.scss'
 
-function Dashboard () {
-	const [data, setData] = useState('')
+function Dashboard() {
+  const [name, setName] = useState('')
+  const [stateImage, setStateImage] = useState(loadingImg)
 
 	useEffect(() => {
-	  Axios.get('/getUser').then(response => {
-	    setData(response.data)
+    Axios.get('/getUser').then(response => {
+      setName(response.data.Name.split(' ')[0])
+      setStateImage(response.data.thumbnail)
 	  })
 	}, [])
 
@@ -19,7 +23,7 @@ function Dashboard () {
         </div>
         <div>
           <button className='Navbar-btn'>
-          <img src={data.thumbnail}
+          <img className='stateImage' src={stateImage} 
             alt="profile-img"
             height='50px'
             width='50px'
@@ -27,6 +31,15 @@ function Dashboard () {
             />
           </button>
         </div>  
+      </div>
+      <div className="Greeting">
+        <h1>Hello {name}</h1>
+      </div>
+      <div className="Features">
+        <FeatureCard className='feature' FeatureName='Meet'/>
+        <FeatureCard className='feature' FeatureName='Calendar'/>
+        <FeatureCard className='feature' FeatureName='Todo'/>
+        <FeatureCard className='feature' FeatureName='Whiteboard'/>
       </div>
     </>
   )
--- client/src/components/FeatureCard/FeatureCard.jsx
+++ client/src/components/FeatureCard/FeatureCard.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './FeatureCard.scss'
+import feature_bg from '../../assets/feature_bg.jpg'
+import meet from '../../assets/meet.png'
+import calendar from '../../assets/calendar.png'
+import todo from '../../assets/todo.png'
+import whiteboard from '../../assets/whiteboard.png'
 
-function FeatureCard () {
+function FeatureCard(props) {
+
+  const [featureImgLink, setFeatureImgLink] = useState(null)
+
+  useEffect(
+    () => {
+      if (props.FeatureName === 'Meet') {
+        setFeatureImgLink(meet)
+      }
+      else if (props.FeatureName === 'Calendar') {
+        setFeatureImgLink(calendar)
+      }
+      else if (props.FeatureName === 'Todo') {
+        setFeatureImgLink(todo)
+      }
+      else {
+        setFeatureImgLink(whiteboard)
+      }
+    }, [props.FeatureName]
+  )
+
   return (
-    <div>
-      <div className='FeatureCard' />
-    </div>
+    <>
+      <div className='FeatureCard'>
+        <div
+          style={{
+            width: '337px',
+            height: '80px',
+            backgroundImage: `url(${feature_bg})`,
+            backgroundSize: 'cover'
+          }}
+				/>
+        <div className='FeatureCardHeader'>
+          <img src={featureImgLink} alt={props.FeatureName} />
+          <h1>{props.FeatureName}</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
