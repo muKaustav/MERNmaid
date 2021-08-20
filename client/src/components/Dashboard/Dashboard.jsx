6fd13ca3eda2e84e10e1726f3f2bf78524ee62b3
Gautamshahkiit <2029165@kiit.ac.in>
2021-08-20 21:49:24 +0530

    still css error

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,48 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
-import loadingImg from '../../assets/profile_logo.png'
-import FeatureCard from '../FeatureCard/FeatureCard'
-import './dashboard.scss'
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import loadingImg from "../../assets/profile_logo.png";
+import FeatureCard from "../FeatureCard/FeatureCard";
+import "./dashboard.scss";
+import { DropdownData } from "./DropdownData";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  const [name, setName] = useState('')
-  const [stateImage, setStateImage] = useState(loadingImg)
+  const [name, setName] = useState("");
+  const [stateImage, setStateImage] = useState(loadingImg);
 
-	useEffect(() => {
-    Axios.get('/getUser').then(response => {
-      setName(response.data.Name.split(' ')[0])
-      setStateImage(response.data.thumbnail)
-	  })
-	}, [])
+  useEffect(() => {
+    Axios.get("/getUser").then((response) => {
+      setName(response.data.Name.split(" ")[0]);
+      setStateImage(response.data.thumbnail);
+    });
+  }, []);
+
+  const [dropdown, setDropdown] = useState(false);
+  const showDropdown = () => setDropdown(!dropdown);
 
   return (
     <>
-      <div className='Navbar'>
+      <div className="Navbar">
         <div className="Navbar-logoBox">
           <h1 className="Navbar-logo">MERNmaid</h1>
         </div>
         <div>
-          <button className='Navbar-btn'>
-          <img className='stateImage' src={stateImage} 
-            alt="profile-img"
-            height='50px'
-            width='50px'
-            referrerPolicy="no-referrer"
+          <button className="Navbar-btn" onClick={showDropdown}>
+            <img
+              className="stateImage"
+              src={stateImage}
+              alt="profile-img"
+              height="50px"
+              width="50px"
+              referrerPolicy="no-referrer"
             />
           </button>
-        </div>  
+        </div>
+        <nav className={dropdown ? "dropdown active" : "dropdown"}>
+          <ul className="dropdown-items" onClick={showDropdown}>
+            <li className="dropdown-toggle">
+              <Link to="#" className="dropdown-bars">
+                <span>{stateImage}</span>
+              </Link>
+            </li>
+            {DropdownData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
       <div className="Greeting">
         <h1 className="Greeting-text">Hey {name}</h1>
       </div>
       <div className="Features">
-        <FeatureCard className='feature' FeatureName='Meet'/>
-        <FeatureCard className='feature' FeatureName='Calendar'/>
-        <FeatureCard className='feature' FeatureName='To-do'/>
-        <FeatureCard className='feature' FeatureName='Whiteboard'/>
+        <FeatureCard className="feature" FeatureName="Meet" />
+        <FeatureCard className="feature" FeatureName="Calendar" />
+        <FeatureCard className="feature" FeatureName="To-do" />
+        <FeatureCard className="feature" FeatureName="Whiteboard" />
       </div>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
---
+++ client/src/components/Dashboard/DropdownData.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import * as CgIcons from "react-icons/cg";
+
+
+export const DropdownData = [
+  {
+    title: "Profile",
+    path: "/",
+    icon: <CgIcons.CgProfile />,
+    cName: "profile",
+  },
+  {
+    title: "Support",
+    path: "/",
+    icon: <CgIcons.CgProfile />,
+    cName: "support",
+  },
+  {
+    title: "Feedback",
+    path: "/",
+    icon: <CgIcons.CgProfile />,
+    cName: "feedback",
+  },
+  {
+    title: "Logout",
+    path: "/",
+    icon: <CgIcons.CgProfile />,
+    cName: "logout",
+  }
+];
