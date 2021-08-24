16ab3191f27ccee07cd933e5e84184ecd53a9636
Gautamshahkiit <2029165@kiit.ac.in>
2021-08-24 09:37:06 +0530

    even with og main

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,58 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
-import loadingImg from '../../assets/profile_logo.png'
-import FeatureCard from '../FeatureCard/FeatureCard'
-import DropdownData from './DropdownData'
-import './dashboard.scss'
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import loadingImg from "../../assets/profile_logo.png";
+import FeatureCard from "../FeatureCard/FeatureCard";
+import DropdownData from "./DropdownData";
+import "./dashboard.scss";
 
-function Dashboard () {
-  const [name, setName] = useState('')
-  const [stateImage, setStateImage] = useState(loadingImg)
-  const [dropdown, setDropdown] = useState(false)
-  const showDropdown = () => setDropdown(!dropdown)
+function Dashboard() {
+  const [name, setName] = useState("");
+  const [stateImage, setStateImage] = useState(loadingImg);
+  const [dropdown, setDropdown] = useState(false);
+  const showDropdown = () => setDropdown(!dropdown);
 
   useEffect(() => {
-    Axios.get('/getUser').then(response => {
-      setName(response.data.Name.split(' ')[0])
-      setStateImage(response.data.thumbnail)
-    })
-  }, [])
+    Axios.get("/getUser").then((response) => {
+      setName(response.data.Name.split(" ")[0]);
+      setStateImage(response.data.thumbnail);
+    });
+  }, []);
 
   return (
     <div>
-      <div className='Navbar'>
-        <div className='Navbar-logoBox'>
-          <h1 className='Navbar-logo'>MERNmaid</h1>
+      <div className="Navbar">
+        <div className="Navbar-logoBox">
+          <h1 className="Navbar-logo">MERNmaid</h1>
         </div>
         <div>
-          <button className='Navbar-btn' onClick={showDropdown}>
+          <button className="Navbar-btn" onClick={showDropdown}>
             <img
-              className='stateImage'
+              className="stateImage"
               src={stateImage}
-              alt='profile-img'
-              height='50px'
-              width='50px'
-              referrerPolicy='no-referrer'
-						/>
+              alt="profile-img"
+              height="50px"
+              width="50px"
+              referrerPolicy="no-referrer"
+            />
           </button>
         </div>
       </div>
-      <div className={dropdown ? 'Dropdown Active' : 'Dropdown'}>
+      <div className={dropdown ? "Dropdown Active" : "Dropdown"}>
         <DropdownData />
       </div>
-      <div className='Greeting'>
-        <h1 className='Greeting-text'>
-					Hey {name}
-        </h1>
+      <div className="Greeting">
+        <h1 className="Greeting-text">Hey {name}</h1>
       </div>
-      <div className='Features'>
-        <FeatureCard className='feature' FeatureName='Meet' />
-        <FeatureCard className='feature' FeatureName='Calendar' />
-        <FeatureCard className='feature' FeatureName='To-do' />
-        <FeatureCard className='feature' FeatureName='Whiteboard' />
+      <div className="Features">
+        <FeatureCard className="feature" FeatureName="Meet" />
+        <FeatureCard className="feature" FeatureName="Calendar" />
+        <FeatureCard className="feature" FeatureName="To-do" />
+        <FeatureCard className="feature" FeatureName="Whiteboard" />
       </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard;
--- client/src/components/Dashboard/DropdownData.jsx
+++ client/src/components/Dashboard/DropdownData.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import profileImg from './DropdownAssets/profile.png'
-import supportImg from './DropdownAssets/support.png'
-import feedbackImg from './DropdownAssets/feedback.png'
-import logoutImg from './DropdownAssets/logout.png'
-import { Link } from 'react-router-dom'
-import './dashboard.scss'
+import React from "react";
+import profileImg from "./DropdownAssets/profile.png";
+import supportImg from "./DropdownAssets/support.png";
+import feedbackImg from "./DropdownAssets/feedback.png";
+import logoutImg from "./DropdownAssets/logout.png";
+import { Link } from "react-router-dom";
+import "./dashboard.scss";
 
-function DropdownData () {
+function DropdownData() {
   return (
     <>
-      <Link className='DropdownContent' key='Profile' to='/profile'>
-          <img src={profileImg} alt='img' />
-          <h1>Profile</h1>
-      </Link>
-      
-      <Link className='DropdownContent' key='Feedback' to='/feedback'>
-          <img src={feedbackImg} alt='img' />
-          <h1>Feedback</h1>
+      <Link className="DropdownContent" key="Profile" to="/profile">
+        <img src={profileImg} alt="img" />
+        <h1>Profile</h1>
       </Link>
 
-      <Link className='DropdownContent' key='Support' to='/support'>
-          <img src={supportImg} alt='img' />
-          <h1>Support</h1>
+      <Link className="DropdownContent" key="Feedback" to="/feedback">
+        <img src={feedbackImg} alt="img" />
+        <h1>Feedback</h1>
       </Link>
 
-      <Link className='DropdownContent' key='Logout'
+      <Link className="DropdownContent" key="Support" to="/support">
+        <img src={supportImg} alt="img" />
+        <h1>Support</h1>
+      </Link>
+
+      <Link
+        className="DropdownContent"
+        key="Logout"
         onClick={() => {
-        window.location.href = 'https://mernmaid.herokuapp.com//logout'
-      }}>
-          <img src={logoutImg} alt='img' />
-          <h1>Logout</h1>
+          window.location.href = "https://mernmaid.herokuapp.com//logout";
+        }}
+      >
+        <img src={logoutImg} alt="img" />
+        <h1>Logout</h1>
       </Link>
     </>
-  )
+  );
 }
 
-export default DropdownData
+export default DropdownData;
